78fd548dfd751af8007bae58f25e4b882fd34144
Mansi <107349957+Mannxxx@users.noreply.github.com>
2023-10-04 11:08:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
-Renders the website using Streamlit. Run this for the website.
+Renders the website using streamlit. Run this for website.
 """
 
 import streamlit as st
-from src.roadmapgpt import ai
+import streamlit.components.v1 as components
+
+from src.roadmapgpt import ai  # exposes the LLM endpoints to streamlit 
 from src.roadmapgpt.utils import (output_sanitizer, dict_to_mermaid, store_to_gsheet)
 
 api_key = st.secrets.TOKEN
@@ -22,7 +24,9 @@ def mermaid(code: str) -> None:
             import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@latest/dist/mermaid.esm.min.mjs';
             mermaid.initialize({{ startOnLoad: true }});
         </script>
-        """
+        """,
+        # TODO : There must be a better way, something like pinch to zoom. Now the website is huge when rendered. 
+        width = 5000, height = 5000
     )
 
 def display(user_prompt, content):
@@ -34,21 +38,21 @@ def display(user_prompt, content):
         store_to_gsheet(content, user_prompt=user_prompt, error=True, **st.secrets.gspread_credentials)
     else:
         output = dict_to_mermaid(output)
-        store_to_gsheet(content, user_prompt=user_prompt, error=False, **st.secrets.gspread_credentials)
+        store_to_gsheet(content, user_prompt = user_prompt, error = True, **st.secrets.gspread_credentials)        # passing google sheet credentials
         mermaid(output)
 
 st.set_page_config(layout="wide")
 
 with st.sidebar:
     st.markdown("""
-        # RoadmapGPT 🗺️
-        RoadmapGPT aims to give you a customized and comprehensive roadmap for all topics, helping you learn faster and better.
+        # RoadmapGPT 🗺️\n
+        RoadmapGPT aims to give you a customized and comprehensive roadmap for all topics, helping you learn faster and better.\n
         Enter a keyword for one domain you would like to know more about.
     """)
 
 with st.form("Input form"):
     with st.sidebar:
-        user_prompt = st.sidebar.text_input(label="Enter a domain to generate roadmap", placeholder="Machine Learning, Chess ..")
+        user_prompt = st.sidebar.text_input(label = "Enter a domain to generate roadmap", placeholder = "Machine Learning, Chess ..")
         submitted = st.form_submit_button("Submit")
 
     if submitted and user_prompt != '':
@@ -58,13 +62,13 @@ with st.form("Input form"):
 with st.sidebar:
     st.markdown("""
         ---
-
-        __General Tips:__
-        If the output fails to parse, try a similar sounding different prompt.
+                
+        __General Tips:__\n
+        If the output fails to parse, try a similar sounding different prompt.\n
         Try Machine Learning advanced, introduction to c++ and so on to get personalized roadmaps.
-
+                
         ---
 
-        We are Open Source!
-        To contribute, please see the [GitHub repo](https://github.com/robinroy03/RoadmapGPT)
+        We are Open Source!\n
+        To contribute, please see the [github repo](https://github.com/robinroy03/RoadmapGPT) 
     """)
